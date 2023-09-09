ae950836d7c38bc6b7a5f719c299eb82fa9f9dc4
retr0 <retr0lbb@gmail.com>
2023-09-08 21:28:11 -0300

    up to final version

--- main.js
+++ main.js
@@ -1,25 +1,49 @@
 const paths = document.querySelectorAll("svg path");
 const nameDisplay = document.querySelector("#nameCountry")
+const imgaeDisplay = document.querySelector("#imageDisplay")
 
-
-
-
-const getAllContryes = () =>{
-    fetch("https://restcountries.com/v3.1/all").then((response => response.json())).then(data=> console.log(data))
+class Country{
+    constructor(name, lenguage, code, flag = ""){
+        this.name = name
+        this.lenguage = lenguage
+        this.code = code;
+        this.flag = flag
+    }
 }
-getAllContryes()
 
-const pathNames ={}
 paths.forEach((path, index) =>{
-    console.log(path.id)
-    const pathName = path.getAttribute("name")
+    const pathName = path.getAttribute("name");
+    const Id = path.getAttribute("id");
     let name;
     if(pathName){
-        name = pathName
+        name = pathName;
     }else{
-        name = path.classList.value
+        name = path.classList.value;
     }
-    path.addEventListener("mouseenter", ()=>{
-        nameDisplay.innerHTML = name
+    try{
+        fetch(`https://restcountries.com/v3.1/alpha/${Id}`)
+        .then((response => {return response.json()}))
+        .then(data =>{
+            const dataCountry = data[0]
+            const country = new Country(name, dataCountry.languages , Id, dataCountry.flags.png);
+
+
+            path.addEventListener("mouseover", (event)=>{
+                nameDisplay.innerHTML = country.name
+                imgaeDisplay.src = country.flag
+            })
+        }).catch(() =>{
+            console.log("erro")
+        }
+        )        
+    }catch{
+        console.log("Não foi possivel fazer isso");
+    }
+    path.addEventListener("mouseout", (event)=>{
+        nameDisplay.innerHTML = ""
+        imgaeDisplay.src = ""
     })
 })
+
+
+
